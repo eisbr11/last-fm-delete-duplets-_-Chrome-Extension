1ef3eecc9271e37065e31e19463ad857b955dc1a
eisbr11 <eisbr11@gmail.com>
2013-05-30 11:18:16 +0200

    changed Contentscript to match style-changes of last.fm

--- javascript/contentscript.js
+++ javascript/contentscript.js
@@ -1,7 +1,7 @@
 // Code for deleting duplets in lastfm
 jQuery.noConflict();
 var current_track_id = "";
-jQuery('#content table#deletablert').children().children().each(function(){
+jQuery('article.content #deletablert').children().children().each(function(){
     if (current_track_id == jQuery(this).attr("data-track-id")) {
         jQuery(this).children('td.deleteCell').children().first().click();
     } else {
